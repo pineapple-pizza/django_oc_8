cc0c5c29fc18d2e617d9cf07df4b176fa00355d9
an.aslanian <an.aslanian@gmail.com>
2021-02-08 17:31:42 +0100

    cors fix

--- api/settings.py
+++ api/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['https://pur-beurre-front.herokuapp.com/',]
 
 AUTH_USER_MODEL = 'authentication.User'
 
