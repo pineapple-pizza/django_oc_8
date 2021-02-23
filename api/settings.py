eaaeebcb7082585053171b795403abc95bfff3f4
an.aslanian <an.aslanian@gmail.com>
2021-02-23 17:35:12 +0100

    test travis

--- api/settings.py
+++ api/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
+        'NAME': '$DB_NAME',
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST')
